909158d371d95b083698a933da9e3a73be84feb9
Guilherme Moreira <guilherme.moreira@addvolt.com>
2024-05-30 15:12:35 +0100

    added visualization and exploration

---
+++ visualization/exploration.py
@@ -0,0 +1,88 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import glob
+
+# Directory containing the CSV files
+csv_folders_path = [
+    "results/200_1000_bs_10_50_8_rpmxo_rrm_1_10_30_10/*.csv",
+    "results/200_1000_bs_10_50_8_rpmxo_rsm_1_10_30_10/*.csv",
+    "results/200_1000_bs_10_50_8_rpmxo_rim_1_10_30_10/*.csv",
+    "results/200_1000_bs_10_50_8_rspxo_rrm_1_10_30_10/*.csv",
+    "results/200_1000_bs_10_50_8_rspxo_rsm_1_10_30_10/*.csv",
+    "results/200_1000_bs_10_50_8_rspxo_rim_1_10_30_10/*.csv",
+    "results/200_1000_bs_10_50_8_uxo_rrm_1_10_30_10/*.csv",
+    "results/200_1000_bs_10_50_8_uxo_rsm_1_10_30_10/*.csv",
+    "results/200_1000_bs_10_50_8_uxo_rim_1_10_30_10/*.csv",
+    "results/200_1000_fps_10_50_8_rpmxo_rrm_1_10_30_10/*.csv",
+    "results/200_1000_fps_10_50_8_rpmxo_rsm_1_10_30_10/*.csv",
+    "results/200_1000_fps_10_50_8_rpmxo_rim_1_10_30_10/*.csv",
+    "results/200_1000_fps_10_50_8_rspxo_rrm_1_10_30_10/*.csv",
+    "results/200_1000_fps_10_50_8_rspxo_rsm_1_10_30_10/*.csv",
+    "results/200_1000_fps_10_50_8_rspxo_rim_1_10_30_10/*.csv",
+    "results/200_1000_fps_10_50_8_uxo_rrm_1_10_30_10/*.csv",
+    "results/200_1000_fps_10_50_8_uxo_rsm_1_10_30_10/*.csv",
+    "results/200_1000_fps_10_50_8_uxo_rim_1_10_30_10/*.csv",
+    "results/200_1000_ts_10_50_8_rpmxo_rrm_1_10_30_10/*.csv",
+    "results/200_1000_ts_10_50_8_rpmxo_rsm_1_10_30_10/*.csv",
+    "results/200_1000_ts_10_50_8_rpmxo_rim_1_10_30_10/*.csv",
+    "results/200_1000_ts_10_50_8_rspxo_rrm_1_10_30_10/*.csv",
+    "results/200_1000_ts_10_50_8_rspxo_rsm_1_10_30_10/*.csv",
+    "results/200_1000_ts_10_50_8_rspxo_rim_1_10_30_10/*.csv",
+    "results/200_1000_ts_10_50_8_uxo_rrm_1_10_30_10/*.csv",
+    "results/200_1000_ts_10_50_8_uxo_rsm_1_10_30_10/*.csv",
+    "results/200_1000_ts_10_50_8_uxo_rim_1_10_30_10/*.csv",
+]
+
+# List to hold data from each file
+df_main = pd.DataFrame()
+
+for csv_folder in csv_folders_path:
+    df_folder = pd.DataFrame()
+    for file in glob.glob(csv_folder):
+        df_file = pd.read_csv(file)
+
+        parts = file.split("_")
+        crossover_method = parts[6]
+        selection_method = parts[2]
+        mutation_method = parts[7]
+
+        final_generation = df_file["Generation"].iloc[-1]
+
+        df_folder = pd.concat(
+            [
+                df_folder,
+                pd.DataFrame(
+                    {
+                        "crossover": crossover_method,
+                        "selection": selection_method,
+                        "mutation": mutation_method,
+                        "generations": final_generation,
+                        "solved": final_generation < 1000,
+                    },
+                    index=[0],
+                ),
+            ],
+        )
+
+    df_main = pd.concat([df_main, df_folder])
+
+grouped_df = (
+    df_main.groupby(["selection", "crossover", "mutation"])
+    .agg(
+        generations_mean=("generations", "mean"),  # Mean of generations
+        solved_count=("solved", lambda x: x.sum()),  # Count of True values in solved
+    )
+    .reset_index()
+    .sort_values("generations_mean")
+)
+
+grouped_df.columns = [
+    "Selection",
+    "Crossover",
+    "Mutation",
+    "Average Generations",
+    "Solved Puzzles",
+]
+
+print(grouped_df.to_markdown(index=False))
